0e158761c54e6a3c1ead1781b8dd349cb8510360
Ivan <zjw@192-168-1-9.tpgi.com.au>
2016-10-27 00:56:16 +1100

    Initial project created.

---
+++ babyMonitor/babyMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babyMonitor
+//
+//  Created by zjw on 27/10/16.
+//  Copyright © 2016 FIT5140. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
